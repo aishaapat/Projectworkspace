03d622748dcd31b4ad36b9e57cc182b67722a736
aishaapat <aishapatel0703@gmail.com>
2024-12-15 11:14:18 -0500

    added register button with same style and created object in the log in pane

--- src/main/java/org/example/projectworkspace/Buttonclasses/Login.java
+++ src/main/java/org/example/projectworkspace/Buttonclasses/Login.java
@@ -1,20 +1,20 @@
 package org.example.projectworkspace.Buttonclasses;
 import javafx.event.ActionEvent;
 
-import java.sql.Connection;
-import java.sql.Statement;
+//import java.sql.Connection;
+//import java.sql.Statement;
 
 public class Login
 {
-    private Connection connection;
-    private Statement statement;
+   // private Connection connection;
+    //private Statement statement;
 
-    public boolean userlogincheck(){
-        this.connection = connection;
-        if(connection != null){
-            System.out.println("Successful connection");
-        }
-        boolean b=true;
-        return b;
-    }
+    //public boolean userlogincheck(){
+       // this.connection = connection;
+        //if(connection != null){
+            //System.out.println("Successful connection");
+       // }
+       // boolean b=true;
+        //return b;
+    //}
 }
--- src/main/java/org/example/projectworkspace/GUI/TestScreen.java
+++ src/main/java/org/example/projectworkspace/GUI/TestScreen.java
@@ -4,6 +4,7 @@ package org.example.projectworkspace.GUI;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.Button;
@@ -24,27 +25,34 @@ public class TestScreen extends Application {
     @Override
     public void start(Stage stage) {
         GridPane root = new GridPane();
+        root.getStyleClass().add("background-primary");
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setAlignment(Pos.CENTER);
         label1 = new Label("Flying Easy!");
+        label1.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white; -fx-font-size: 24px; -fx-padding: 15px 30px; -fx-border-radius: 5px; -fx-background-radius: 5px;");
         label2 = new Label("Username:");
         label3 = new Label("Password:");
+
 
         text1 = new TextField();
         text2 = new TextField();
 
         // Add controls to the grid
-        root.add(label1, 0, 0, 2, 1);  // spanning over two columns for centering
-        root.add(label2, 0, 1);
-        root.add(text1, 1, 1);
-        root.add(label3, 0, 2);
-        root.add(text2, 1, 2);
+        root.add(label1, 0, 0, 2, 1);
+
+        root.add(label2, 3, 1);
+        root.add(text1, 4, 1);
+        root.add(label3, 3, 2);
+        root.add(text2, 4, 2);
 
         LoginButton loginButton = new LoginButton();
-        root.add(loginButton, 1, 3);
+        root.add(loginButton, 7, 3);
         RegisterButton registerButton = new RegisterButton();
-        root.add(registerButton, 2, 3);
+        root.add(registerButton, 8, 3);
         root.getChildren();
 
-        Scene scene = new Scene(root, 600, 600);
+        Scene scene = new Scene(root, 600, 400);
 
         stage.setTitle("Welcome to Easy Flying!");
         stage.setScene(scene);
