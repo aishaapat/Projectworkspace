31087f57f580a6b11c2fac0a698b3ce13cd58ae6
aishaapat <aishapatel0703@gmail.com>
2024-12-14 22:55:32 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.projectworkspace {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.projectworkspace to javafx.fxml;
+    exports org.example.projectworkspace;
+}
