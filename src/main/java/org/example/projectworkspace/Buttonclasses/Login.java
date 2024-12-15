65e257fffdca10a8922ef2edd2a04bc0611fdf28
aishaapat <aishapatel0703@gmail.com>
2024-12-14 23:11:00 -0500

    adding a simple method for checking username connection

--- src/main/java/org/example/projectworkspace/Buttonclasses/Login.java
+++ src/main/java/org/example/projectworkspace/Buttonclasses/Login.java
@@ -1,4 +1,20 @@
 package org.example.projectworkspace.Buttonclasses;
 import javafx.event.ActionEvent;
-public class Login {
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class Login
+{
+    private Connection connection;
+    private Statement statement;
+
+    public boolean userlogincheck(){
+        this.connection = connection;
+        if(connection != null){
+            System.out.println("Successful connection");
+        }
+        boolean b=true;
+        return b;
+    }
 }
