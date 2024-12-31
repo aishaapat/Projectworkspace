53ee838a8fe594c05e33ea6a9fe04650f51fed79
aishaapat <aishapatel0703@gmail.com>
2024-12-30 23:11:02 -0500

    added action event for button

--- src/main/java/org/example/projectworkspace/GUI/EnterButton.java
+++ src/main/java/org/example/projectworkspace/GUI/EnterButton.java
@@ -7,29 +7,14 @@ public class EnterButton extends Button
     public EnterButton() {
         super("Enter");
 
-        // Notes
+        // Will add event handler for each specific case
 
         // Styling the button
         this.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white; -fx-font-size: 16px; -fx-padding: 10px 20px;");
 
-        // Setting the event handler for the button click
-        this.setOnAction(event -> handleenter());
+
+
     }
 
-    // Event handler for login button click
-    private void handleenter() {
-        boolean enterpressed = enterreg();
 
-        if (enterpressed) {
-            System.out.println("Enter button pressed");
-        } else {
-            System.out.println("Try again");
-        }
-    }
-
-    // Simulate login logic (replace with actual logic)
-    private boolean enterreg() {
-        // TODO: Implement real login logic here
-        return true; // Simulating a successful login
-    }
 }
--- src/main/java/org/example/projectworkspace/GUI/RegisterButton.java
+++ src/main/java/org/example/projectworkspace/GUI/RegisterButton.java
@@ -6,32 +6,17 @@ public class RegisterButton extends Button
     public RegisterButton() {
         super("Register");
 
-        // Notes
+        // Since this only swtiches scenes we will put event handler for the needed screen
 
         // Styling the button
         this.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white; -fx-font-size: 16px; -fx-padding: 10px 20px;");
 
-        // Setting the event handler for the button click
-        this.setOnAction(event -> handleregister());
+
     }
 
-    // Event handler for login button click
-    private void handleregister() {
-        boolean registerpressed = performregister();
-
-        if (registerpressed) {
-            System.out.println("Register button pressed");
 
 
-        } else {
-            System.out.println("Try again");
-        }
-    }
 
-    // Simulate login logic (replace with actual logic)
-    private boolean performregister() {
-        // TODO: Implement real login logic here
-        return true; // Simulating a successful login
-    }
+
 
 }
