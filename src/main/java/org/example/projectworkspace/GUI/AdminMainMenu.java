53c9eab1f2e524443fddbdc4e0fc44865ffda96c
aishaapat <aishapatel0703@gmail.com>
2025-01-02 12:31:15 -0500

    pull this too

--- src/main/java/org/example/projectworkspace/GUI/AdminMainMenu.java
+++ src/main/java/org/example/projectworkspace/GUI/AdminMainMenu.java
@@ -2,6 +2,7 @@ package org.example.projectworkspace.GUI;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,10 +10,13 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class AdminMainMenu extends Application {
+public class AdminMainMenu extends Application implements EventHandler<ActionEvent> {
 
     Stage stage;
     String FirstName = "username"; // Replace with actual firstname of user
+    // adding this to the top of the class because I am using eventhandler for this
+    Button ManagePageButton = new Button("Manage Page");
+
 
     @Override
     public void start(Stage stage) {
@@ -31,7 +35,6 @@ public class AdminMainMenu extends Application {
         // Buttons for Manage Bookings, Logout, and a special button for managing flights
         Button manageBookingsButton = new Button("Manage Bookings");
         Button logoutButton = new Button("Logout");
-        Button ManageFlights = new Button("Manage Flights");
 
         // Action for Manage Bookings button
         // :: is a method reference used to shorten action handlers like
@@ -41,6 +44,7 @@ public class AdminMainMenu extends Application {
         // Action for Logout button
         logoutButton.setOnAction(this::handleLogout);
 
+        ManagePageButton.setOnAction(this);
         // Action for Manage Flights Button
         // TODO : Add method handler for -> ManageFlights.setOnAction(this::handleManageFlights);
 
@@ -50,7 +54,7 @@ public class AdminMainMenu extends Application {
 
         root.add(manageBookingsButton, 0, 1);
         root.add(logoutButton, 1, 1);
-        root.add(ManageFlights, 2, 1);
+        root.add(ManagePageButton, 2, 1);
 
         // Set up the scene and stage
         Scene scene = new Scene(root, 400, 200);
@@ -75,8 +79,27 @@ public class AdminMainMenu extends Application {
         loginScreen.start(new Stage()); // Open the Login screen
         stage.close(); // Close the current Main Menu screen
     }
+    private void handleManage(ActionEvent event) throws Exception {
+        System.out.println("Going to edit screen..");
+        AdminEditScreen adminEditScreen = new AdminEditScreen();
+        adminEditScreen.start(new Stage());
+        stage.close();
+    }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        if(actionEvent.getSource()==ManagePageButton){
+            AdminEditScreen adminEditScreen = new AdminEditScreen();
+            try {
+                adminEditScreen.start(new Stage());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            stage.close();
+        }
     }
 }
